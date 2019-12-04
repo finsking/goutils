41e3b40985e0e8cb5d72a075797811fbd4ae4d64
王峰 <wangfeng@pheicloud.com>
2019-12-04 17:42:53 +0800

    common logger

--- logger/logger.go
+++ logger/logger.go
@@ -1,7 +1,29 @@
 package logger
 
-import "fmt"
+import (
+    "fmt"
+    "strings"
+)
 
-func LogTest() {
-    fmt.Println("Logger Test")
+var DebugMode bool
+
+func SetDebugMode(debug bool) {
+    DebugMode = debug
+}
+
+//Display in debug mode
+func D(msg ...interface{}) {
+    if DebugMode {
+        fmt.Println("[DEBUG]", strings.Trim(fmt.Sprint(msg), "[]"))
+    }   
+}
+
+//Display info
+func I(msg ...interface{}) {
+    fmt.Println("[INFO]", strings.Trim(fmt.Sprint(msg), "[]"))
+}
+
+//Display error
+func E(msg ...interface{}) {
+    fmt.Println("[ERROR]", strings.Trim(fmt.Sprint(msg), "[]"))
 }
